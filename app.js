f554f4210e9056caad85564a0db9a92490d024de
Gurpreet Chugh <chugh.gurpreet@gmail.com>
2023-05-29 09:44:59 -0400

    Commit

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-let samplesData = d3.json('https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json').then()
 
-let samplesID = samplesData['names']
-console.log(samplesID[0])
+d3.json('https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json')
+.then(function(data) {
+    let samplesData = data
+    console.log(samplesData)
+    let samplesID = samplesData.names
+    console.log(samplesID[0])
+    let samplesDemographics = samplesData.metadata
+    console.log(samplesDemographics[0])
+    let samplesPlotData = samplesData.samples
+    console.log(samplesPlotData[0])
+    let traceBar = {
+        x: samplesPlotData[0].sample_values.slice(0,10),
+        y: samplesPlotData[0].otu_ids,
+        type: 'bar',
+        orientation: 'h',
+        width: 100,
+        hovertext: samplesPlotData[0].otu_labels
+    }
 
-let samplesDemographics = samplesData['metadata']
-console.log(samplesDemographics[0])
+    let layoutBar = {
+        xaxis: {automargin: true}
+    }
+    Plotly.newPlot('bar', [traceBar], layoutBar)
+})
 
-let samplesPlotData = samplesData['samples']
+
+
+
+
+
+
