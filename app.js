c01d3a0eaa3d7e17ac43f8c732641b2cd73b203a
Gurpreet Chugh <chugh.gurpreet@gmail.com>
2023-05-31 12:12:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,23 @@
 d3.json('https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json')
   .then(data => {
     let samplesData = data;
+    console.log(samplesData);
+    getOptionValues(samplesData);
     init(samplesData);
 })
+
+// Creating function to insert dropdown values at id="selDataset"
+
+function getOptionValues(my_array) {
+  d3.select('#selDataset')
+    .selectAll('option')
+    .data(my_array.names)
+    .enter()
+    .append('option')
+    .attr('values', value => value
+    )
+    .text(value => value)
+}
 
 // Creating function to initialize default charts and data to display
 function init(data)  {
@@ -35,8 +50,7 @@ function init(data)  {
         x: sampleValues.slice(0,10).reverse(),
         y: sampleLabels.slice(0,10).reverse(),
         type: 'bar',
-        orientation: 'h',
-        // width: 1,
+        orientation: 'h',       
         text: samplePlotData.otu_labels
     }
 
@@ -44,13 +58,28 @@ function init(data)  {
             xaxis: {automargin: true},
             width: 800,
             height: 600
-        }
+    }
+
     Plotly.newPlot('bar', [traceBar], layoutBar)
 
-    let traceBubble = {}
-}
+    let traceBubble = {
+        x: samplePlotData.otu_ids,
+        y: samplePlotData.sample_values,
+        mode: 'markers',
+        marker: {
+          size: samplePlotData.sample_values,
+          color: samplePlotData.otu_ids
+        },
+        text: samplePlotData.otu_labels
+    }
 
-// Creating function
+    let layoutBubble = {
+      width: 800,
+      height: 600
+    }
+
+    Plotly.newPlot('bubble', [traceBubble], layoutBubble)
+}
 
 
 
